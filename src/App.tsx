b3c1eec28c902921d40f83c6150a7e0e85b1a316
Sam Sheets <sam.sheets94@gmail.com>
2023-04-18 09:42:01 -0600

    add Links

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes } from 'react-router-dom';
 import './App.css';
 import { Home } from './Home';
 import { About } from './About';
 
 export const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />}/>
-      <Route path='about' element={<About />} />
-    </Routes>
+    <>
+      <Link to='/' style={{ textDecoration: 'none' }}>Home</Link>
+      <Link to='/about' style={{ textDecoration: 'none' }}>About</Link>
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='about' element={<About />} />
+      </Routes>
+    </>
   );
 }
-
-
