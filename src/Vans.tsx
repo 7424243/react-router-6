3aeb861c23e4c7c9a731c65e07062763a27cad79
Sam Sheets <sam.sheets94@gmail.com>
2023-04-19 15:08:40 -0600

    add miragejs server and fetch vans data

--- src/Vans.tsx
+++ src/Vans.tsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from 'react';
+
 export const Vans = () => {
+    const [data, setData] = useState<any>([])
+
+    const fetchData = async () => {
+        const res = await fetch('/api/vans');
+        const jsonData = await res.json();
+        setData(jsonData.vans)
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
     return (
-        <h1>Vans Page Goes Here</h1>
+        <>
+            <h1>Explore Our Van Options</h1>
+            <ul style={{display:'flex', flexDirection: 'row', flexWrap: 'wrap', paddingLeft: 0}}>
+                {data.map((van: any) => (
+                    <li key={van.id} style={{listStyleType: 'none', marginBottom: '1.5%', marginRight: '1%'}}>
+                        <div>
+                            <img alt={van.name} src={van.imageUrl} style={{width: 300, height: 300}}/>
+                        </div>
+                        <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                            <h2>{van.name}</h2>
+                            <p>${van.price} / day</p>
+                        </div>
+                        <p style={{borderStyle: 'solid', borderColor: 'grey', borderWidth: 2, borderRadius: '10%', width: 75}}>{van.type}</p>
+                    </li>
+                ))}
+            </ul>
+        </>
     )
 }
