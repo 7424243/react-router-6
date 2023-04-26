5e006d0f61495544f759ec5ddee25e8579c7d1a3
Sam Sheets <sam.sheets94@gmail.com>
2023-04-26 08:32:46 -0600

    add createBrowserRouter setup and rm App component files

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App } from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import "./server";
+import { HostLayout } from './components/HostLayout';
+import { Layout } from './components/Layout';
+import { About } from './pages/About';
+import { Home } from './pages/Home';
+import { Dashboard } from './pages/Host/Dashboard';
+import { HostVanDetail } from './pages/Host/HostVanDetail';
+import { HostVanDetailLayout } from './pages/Host/HostVanDetailLayout';
+import { HostVanPhotos } from './pages/Host/HostVanPhotos';
+import { HostVanPricing } from './pages/Host/HostVanPricing';
+import { HostVans } from './pages/Host/HostVans';
+import { Income } from './pages/Host/Income';
+import { Reviews } from './pages/Host/Reviews';
+import { NotFound } from './pages/NotFound';
+import { VanDetail } from './pages/Vans/VanDetail';
+import { Vans } from './pages/Vans/Vans';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={<Layout />}>
+      <Route index element={<Home />}/>
+      <Route path='host' element={<HostLayout />}>
+        <Route index element={<Dashboard />} />
+        <Route path='income' element={<Income />} />
+        <Route path='vans' element={<HostVans />} />
+        <Route path='vans/:id' element={<HostVanDetailLayout />}>
+          <Route index element={<HostVanDetail />}/>
+          <Route path='pricing' element={<HostVanPricing />}/>
+          <Route path='photos' element={<HostVanPhotos />}/>
+        </Route>
+        <Route path='reviews' element={<Reviews />} />
+      </Route>
+      <Route path='about' element={<About />} />
+      <Route path='vans' element={<Vans />} />
+      <Route path='vans/:id' element={<VanDetail />} />
+      <Route path='*' element={<NotFound />} />
+    </Route>
+))
+
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RouterProvider router={router} />
 );
 
 // If you want to start measuring performance in your app, pass a function
